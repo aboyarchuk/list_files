93d7d92d6f8d7b1f2ae1d88dcfb438227797095c
aboyarchuk <boyarchuk_alina@mail.ru>
2016-01-28 12:39:28 +0200

    Добавл вывод одинак по размеру файлов

--- test4.php
+++ test4.php
@@ -4,13 +4,40 @@ function GetListFiles($folder,&$all_files){
     while($cv_file=readdir($fp)) {
         if(is_file($folder."/".$cv_file)) {
             $all_files[]=$folder."/".$cv_file;
-        }elseif($cv_file!="." && $cv_file!=".." && is_dir($folder."/".$cv_file)){
+        }
+    elseif($cv_file!="." && $cv_file!=".." && is_dir($folder."/".$cv_file)){
             GetListFiles($folder."/".$cv_file,$all_files);
         }
     }
     closedir($fp);
 }
-
+$all_files = array();
 GetListFiles("/home/alina/papka",$all_files);
 print_r($all_files);
+
+echo $all_files[7] . filesize($all_files[7]) . "\n";
+$a = file_get_contents($all_files[17]);
+echo $a . "\n";
+/*
+foreach($all_files ass$value){
+        echo filesize($all_files[$i]) . $value . "\n";
+        $i++
+        }*/
+
+for($i = 0, $b = count($all_files); $i<$b ; $i++) {
+    $razm = array();
+      $razm = filesize($all_files[$i]);
+    $r = array();
+    foreach($razm as $k => $v) {
+            if(isset($r[$v])){
+               $r[$v]++ ;
+               if($r[$v]>=2)
+                  echo 'Files one size' . $v ;
+            }
+            else $r[$v]=1;
+    }
+}
+
+        
+
 ?>
